872b9f803074c03e6f8766285d2fb55543121236
Edgar Malesa <malesaedgar@gmail.com>
2023-09-27 20:38:37 +0200

    complete challenge_2 for IWA_6

---
+++ CHALLENGES/IWA_6/Challenge_2/challenge_2.js
@@ -0,0 +1,23 @@
+const rent = 400;
+const tax = '8%';
+const food = 51.7501;
+const salary = 800;
+const transport = 10.2;
+const hourOfDay = 00;
+const minuteOfDay = 00;
+
+// Only change below this line
+
+let balance;
+
+if ((typeof hourOfDay !== 'undefined') && (typeof minuteOfDay !== 'undefined') && (hourOfDay === 0) && (minuteOfDay === 0)) {
+    const taxAsDecimal = parseFloat(tax) / 100;
+    const salaryAfterTax = salary * (1 - taxAsDecimal); 
+    balance = salaryAfterTax - transport - food - rent;
+}
+
+if (typeof balance !== 'undefined') {
+    console.log("R " + balance.toFixed(2));
+} else {
+    console.log("Time is not known or it's not midnight, balance is not calculated.");
+}
