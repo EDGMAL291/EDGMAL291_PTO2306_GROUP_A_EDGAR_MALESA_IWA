07c81baea6e3f820467ce84d1728b55a2a56beab
Edgar Malesa <malesaedgar@gmail.com>
2023-10-01 07:38:03 +0200

    first one done

---
+++ CHALLENGES/IWA_7/Challenge_1/Challenge_1.js
@@ -0,0 +1,3 @@
+const value = "3"
+console.log(Number(value) + 4 + Number(value)) // added Number() to convert value from string to number. 
+// value is a string. adding a string to a number doesn't give you the sum but concatenate them. 
