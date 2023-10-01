c6664145443f122f73888abc9cb238d5db763058
Edgar Malesa <malesaedgar@gmail.com>
2023-10-01 16:24:04 +0200

    done!

--- CHALLENGES/IWA_7/Challenge_3/Challenge_3.js
+++ CHALLENGES/IWA_7/Challenge_3/Challenge_3.js
@@ -10,15 +10,18 @@ const divider = '----------------------------------'
 
 // Only change below this line
 
+/* used Math.abs() to return positive values, toFixed() for decimal, trim()
+for space. \n to start a on a new line. 
 
-const positiveLeo = Math.abs(parseFloat(leoBalance));
-const positiveSarah = Math.abs(parseFloat(sarahBalance));
+*/
+const positiveLeo = Math.abs(parseFloat(leoBalance)).toFixed(2);
+const positiveSarah = Math.abs(parseFloat(sarahBalance)).toFixed(2);
 const total = parseFloat(positiveLeo) + parseFloat(positiveSarah);
 const owed = total.toFixed(2);
 const totalOwed = `Total amount owed: R ${owed}`;
 const leo = `${leoName.trim()} ${leoSurname.trim()} (Owed: R ${positiveLeo})`;
 const sarah = `${sarahName.trim()} ${sarahSurname.trim()} (Owed: R ${positiveSarah})`;
 
-const result = leo +  sarah + divider + " " + totalOwed +" "+ divider
+const result = `\n${leo}\n${sarah}\n\n${divider}\n  ${totalOwed}  \n${divider}\n`;
 
-console.log(result)
+console.log(result);
