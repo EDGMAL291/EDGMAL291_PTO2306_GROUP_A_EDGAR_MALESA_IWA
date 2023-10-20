d154d78dbfcc3d45896f2839564607efdcd457d6
Edgar Malesa <malesaedgar@gmail.com>
2023-10-20 14:06:25 +0200

    fixed my function calling on line 18

--- CHALLENGES/IWA_13/scripts.js
+++ CHALLENGES/IWA_13/scripts.js
@@ -4,25 +4,28 @@ let calculated = '1'
 
 // Only allowed to change below
 
-
-const logCalc = () => { 
+//function was not properly declared using the arrow function
+const logCalc = () => {
+  //changed the comparison sign from = to === and used string instead of numerical-string 
     const isString = typeof calculated === 'string' 
+    //Used parseInt on the first calcutated change the string ('1') to (1) when calculated === 'string' basically true.
     const calculatedAsNumber = isString ? parseInt(calculated) : calculated
     calculated = calculatedAsNumber + 1 
 }
-
+//function was not properly declared using the arrow function
 const calcUser = () => {
-  logCalc
+  //logCalc was not called so i added () to properly call the function.
+  logCalc()
   if (calculated > 2) user = 'John'
   if (calculated > 2) state = 'requesting'
   if (calculated > 3) state = 'idle'
 }
-
+//function was not properly declared using the arrow function
 const checkUser = () => {
 	if (user && state === 'requesting') {
 		console.log(`User: ${user} (${calculated})`)
-	
-  state = 'stop'
+    //Changed the state to 'stop' to stop the code from running more than once. after the conditions are met the code will stop.
+	  state = 'stop'
   }
 }
 
